917d626ed8cf78444be04fe8916f7c7f783f4410
ehansen247 <ehansen247@gmail.com>
2019-08-12 03:06:18 +0100

    Updates

---
+++ precache-manifest.806c177e3e69def5a091d3b58d637ee2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3d4ae0c0aef9131903faf5cf52b510",
+    "url": "/ReactGames/index.html"
+  },
+  {
+    "revision": "2111265e1103ad95a5c5",
+    "url": "/ReactGames/static/css/main.68621d9f.chunk.css"
+  },
+  {
+    "revision": "2e35611face1971da6a9",
+    "url": "/ReactGames/static/js/2.1c57aca4.chunk.js"
+  },
+  {
+    "revision": "2111265e1103ad95a5c5",
+    "url": "/ReactGames/static/js/main.1de3af03.chunk.js"
+  },
+  {
+    "revision": "141cab769ffdd441845f",
+    "url": "/ReactGames/static/js/runtime~main.5296c498.js"
+  },
+  {
+    "revision": "f4cffc277e4a36b5d461395f47ca26f0",
+    "url": "/ReactGames/static/media/connect4.f4cffc27.png"
+  },
+  {
+    "revision": "65f1551e196b1939bcc6f5be37f84948",
+    "url": "/ReactGames/static/media/ticTacToe.65f1551e.png"
+  }
+]);
